24fe3e4e0b40b46c271e318cf642c08a4c6b9da6
lichenyigit <lichenyiwin@outlook.com>
2017-07-04 18:33:26 +0800

    init

---
+++ src/main/java/decorate/Score.java
@@ -0,0 +1,11 @@
+/**
+ * @author Lichenyi
+ * @date 2017-7-4
+ */
+public abstract class Score {
+    String signature  = "Unkonwn";
+    public String getSignature(){
+        return signature;
+    }
+    public abstract
+}
