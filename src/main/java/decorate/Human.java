7d582f0c3fddde556a3f2982244b8edea4a77099
lichenyigit <lichenyiwin@outlook.com>
2017-07-06 13:55:40 +0800

    装饰模式完善

---
+++ src/main/java/decorate/Human.java
@@ -0,0 +1,7 @@
+/**
+ * @author Lichenyi
+ * @date 2017-7-6
+ */
+public interface Human {
+    public void sing();
+}
--- src/main/java/decorate/Singer.java
+++ src/main/java/decorate/Singer.java
@@ -2,7 +2,8 @@
  * @author Lichenyi
  * @date 2017-7-4
  */
-public class Singer {
+public class Singer implements Human {
+    @Override
     public void sing(){
         System.out.println("我唱唱唱......");
     }
